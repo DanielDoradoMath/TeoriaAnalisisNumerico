b6d063e3dd791bbd6a8ed201258352019e72935b
Daniel Dorado Toro <57468697+DanielDoradoPhys@users.noreply.github.com>
2021-11-11 15:05:56 -0500

    Conjugate gradient

--- Tarea03/conjugate-grad.py
+++ Tarea03/conjugate-grad.py
@@ -13,5 +13,35 @@ import numpy as np
     
 
 
-# def A_prod(x,y):
-#     pass
+def A_prod (x,y, A):
+    prod = 0
+    for i in range(len(x)):
+        for j in range(len(y)):
+            prod += y[j] A[j][i] * x[i]
+    return prod
+
+
+def LinearCG(A, b, x0, eps=1e-5):
+    xk = x0
+    rk = np.dot(A, xk) - b
+    pk = -rk
+    rk_norm = np.linalg.norm(rk)
+    
+    num_iter = 0
+    while rk_norm > eps:
+        apk = np.dot(A, pk)
+        rkrk = np.dot(rk, rk)
+        
+        alpha = rkrk / np.dot(pk, apk)
+        xk = xk + alpha * pk
+        rk = rk + alpha * apk
+        beta = np.dot(rk, rk) / rkrk
+        pk = -rk + beta * pk
+        
+        num_iter += 1
+        curve_x.append(xk)
+        rk_norm = np.linalg.norm(rk)
+        print('Iteration: {} \t x = {} \t residual = {:.4f}'.
+              format(num_iter, xk, rk_norm))
+    
+    print('\nSolution: \t x = {}'.format(xk))
